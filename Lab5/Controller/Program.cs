517577107c5d568fd55c4cc4166e52228c89a456
5 admin <kirkenj@outlook.com>
2020-12-05 15:41:48 +0300

    I've done some my tasks

--- Lab5/Controller/Program.cs
+++ Lab5/Controller/Program.cs
@@ -7,8 +7,8 @@ namespace Lab5
     {
         public static void Main(string[] args)
         {
+            OutInConsole.Print(B5Logic.IsMoreEvenInNum(56));
             OutInConsole.Print(CLogic.ReverseNumber(UserInput.GetInt()));
-            Meow.meow();
         }
     }
 }
---
+++ Lab5/Models/A1Logic.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab5.Models
+{
+    public static class A1Logic
+    {
+        public static int CountDigits(int num)
+        {
+            int counter = 0;
+            while (num > 0)
+            {
+                num /= 10;
+                counter++;
+            }
+            return counter;
+        }
+    }
+}
